021dee173d033cb460a8989d116fef45397a9e0c
odin <odin@nygard.io>
2022-04-20 19:33:29 +0200

    sh file

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 sudo pacman -Suy
 sudo pacman -S git ansible
-git clone https://github.com/Alzaron/Ansible-Arch.git
-cd Ansible-Arch/roles
-ansible-playbook -K main.yml
+ansible-playbook -K roles/main.yml
